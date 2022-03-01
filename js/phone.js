503c119ea0ce0cd2fc2e9bdc5db8e5519192fe72
pushpitanandy <nandypushpita@gmail.com>
2022-03-01 12:38:38 +0600

    fetch add for search phone

--- js/phone.js
+++ js/phone.js
@@ -2,6 +2,9 @@
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
     searchField.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
 }
