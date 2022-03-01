30470d338cb156736915a4fa60f17ce32abbd50d
pushpitanandy <nandypushpita@gmail.com>
2022-03-01 13:06:46 +0600

    card added

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-//button handler
+//search button handler
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -6,5 +6,10 @@ const searchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displaySearchResult(data));
+}
+
+// display phone search results
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
 }
