76347a0e57f94ee518fbf5d821b094c907df50ca
pushpitanandy <nandypushpita@gmail.com>
2022-03-01 17:51:53 +0600

    result cards added

--- js/phone.js
+++ js/phone.js
@@ -6,10 +6,24 @@ const searchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displaySearchResult(data));
+        .then(data => displaySearchResult(data.data));
 }
 
 // display phone search results
 const displaySearchResult = phones => {
     const searchResult = document.getElementById('search-result');
+    phones.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <h6>${phone.brand}</h6>
+                <button class="bg-success text-white p-2">Show Details</button>
+            </div>
+        </div>`;
+        searchResult.appendChild(div);
+    })
 }
